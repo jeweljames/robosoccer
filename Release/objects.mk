55a72bb7a6ba69bbb74dbf3464accc3e2acd4344
Jewel James <mailjeweljames@gmail.com>
2014-12-12 23:33:03 +0530

    porting from Opencv2.9 to 3.0

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_photo -lopencv_imgproc -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_flann -lopencv_ml
+LIBS :=
 
--- src/pose_est.cpp
+++ src/pose_est.cpp
@@ -5,26 +5,29 @@
  *      Author: Jewel James
  */
 
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
 
 using namespace cv;
 using namespace std;
+
 /** @function main */
 int main(int argc, char** argv)
 {
   Mat src, src_gray;
   double t = (double)getTickCount();     //start time
-  /// Read the image
-  src = imread("../data/circles_rot.jpg", 1 );
 
-  if( !src.data )
-    { return -1; }
+  /// Read the image
+
+  src=imread("../data/circles.jpg", 1);
+
+
 
   /// Convert it to gray
-  cvtColor( src, src_gray, CV_BGR2GRAY );
+  //cvtColor( src, src_gray, CV_BGR2GRAY );
+  cvtColor(src, src_gray, COLOR_BGR2GRAY);
 
   /// Reduce the noise so we avoid false circle detection
   GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2 );
@@ -32,8 +35,12 @@ int main(int argc, char** argv)
   vector<Vec3f> circles;
 
   /// Apply the Hough Transform to find the circles
-  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 100, 0, 0 );
-Point indi[2];
+  //HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 100, 0, 0 );
+
+  HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 100 );
+  Point indi[2];
+
+
   /// Draw the circles detected
   for( size_t i = 0; i < circles.size(); i++ )
   {
@@ -46,10 +53,6 @@ Point indi[2];
       circle( src, indi[i], radius, Scalar(0,0,255), 3, 8, 0 );
    }
 
-
-
-
-
 Point centroid;
 centroid.x = (indi[0].x+indi[1].x+indi[2].x)/3;
 centroid.y = (indi[0].y+indi[1].y+indi[2].y)/3;
@@ -58,22 +61,22 @@ circle(src, centroid , 3 , Scalar(0,255,0),-1,8,0);
 double theta = atan((double)(-centroid.y + indi[0].y)/(centroid.x - indi[0].x)) ;
 double theta_degree = theta*180/M_PI;
 
-if (indi[0].x>centroid.x&&indi[0].y>centroid.y)
-{
-  cout<<"a";
-}
-else if (indi[0].x>centroid.x&&indi[0].y<centroid.y)
+if (indi[0].x>centroid.x&&indi[0].y<centroid.y)
  {
- cout<<"b";
- }
-else if (indi[0].x<centroid.x&&indi[0].y>centroid.y)
- {
-   cout<<"c";
+ //nothing happens here,dumbo !
  }
 else if (indi[0].x<centroid.x&&indi[0].y<centroid.y)
  {
-   cout<<"d";
+  theta_degree= theta_degree +180; //second quadrant
  }
+else if (indi[0].x<centroid.x&&indi[0].y>centroid.y)
+ {
+  theta_degree=theta_degree+ 180; //third quadrant
+ }
+else if (indi[0].x>centroid.x&&indi[0].y>centroid.y)
+{
+  theta_degree=theta_degree+ 360; // fourth quadrant
+}
 
 
 line(src,indi[0],indi[1],Scalar( 0, 0, 0 ),2,8 );
@@ -87,7 +90,7 @@ cout << "Times passed in seconds: " << t << endl;
 cout << "orientation" << theta_degree << endl;
 
   /// Show your results
-  namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
+  namedWindow( "Hough Circle Transform Demo", WINDOW_AUTOSIZE );
   imshow( "Hough Circle Transform Demo", src );
 
   waitKey(0);
